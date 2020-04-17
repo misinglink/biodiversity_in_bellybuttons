42f6887bfd1eef9f2c552ad09317b64bc0f35e24
techcheese <cesartole73@gmail.com>
2020-04-17 02:29:53 -0500

    gauge, bubble plot, pie chart

--- static/js/app.js
+++ static/js/app.js
@@ -1,11 +1,11 @@
+const selector = d3.select("#selDataset");
 
-var selector = d3.select("#selDataset");
-
-
-
-const sample_url = 'https://127.0.0.1/sample';
-const metadata_url = 'https://127.0.0.1/metadata';
-
+const zipArrays = (array1, array2) => {
+  zippedArray = array1.map(function(e, i) {
+    return [e, array2[i]];
+  })
+  return zippedArray
+}
 
 function buildMetadata(sample) {
 
@@ -22,22 +22,117 @@ function buildMetadata(sample) {
       metadataTag.append('li').text(key.charAt(0) + key.slice(1).toLowerCase() + ': ' + value)})})}
 
 
-
-
-
-
 function buildCharts(sample) {
   // @TODO: Use `d3.json` to fetch the sample data for the plots
   // @TODO: Build a Bubble Chart using the sample data
   // @TODO: Build a Pie Chart
   // HINT: You will need to use slice() to grab the top 10 sample_values,
   // otu_ids, and labels (10 each).
-  d3.json('/samples/940')
+
+  
+  d3.json('/samples/' + sample).then( (samples) => {
+    var bacteriaNames = []
+    samples['otu_labels'].forEach( (string) => {
+      
+      var splitString = string.split(';')
+      bacteriaNames.push(splitString[splitString.length-1])
+      // bacteriaNames.push(splitString[-1])
+    })
+  labelValueZip = zipArrays(bacteriaNames, samples['sample_values'])
+
+  uniqueSum = {}
+
+  labelValueZip.forEach( (array) => {
+    if (!(array[0] in uniqueSum)) {
+      uniqueSum[array[0]] = array[1];
+    }
+    else {
+      uniqueSum[array[0]] += array[1];
+    }
+
+
+  })
+
+  
+      var trace1 = {
+      x: samples['otu_ids'],
+      y: samples['sample_values'],
+      mode: 'markers',
+      text: bacteriaNames,
+      name: 'Sample: ' + sample,
+      marker: {
+        color: samples['otu_ids'],
+        // // opacity: [1, 0.8, 0.6, 0.4],
+        size: samples['sample_values']
+        
+      }
+    };
+
+    var bubbleData = [trace1];
+    
+    var bubbleLayout = {
+      title: 'Biodiversity in Bellybuttons',
+      height: 700,
+      xaxis: {
+        title: {
+          text: 'Operational taxonomic unit (OTU) ID',
+          font: {
+            family: 'Courier New, monospace',
+            size: 18,
+            color: '#7f7f7f'
+          }
+        },
+      },
+      yaxis: {
+        title: {
+          text: 'Bacteria Units in Thousands',
+          font: {
+            family: 'Courier New, monospace',
+            size: 18,
+            color: '#7f7f7f'
+          }
+        }
+      }
+    };
+
+    var pieData = [{
+      values: Object.values(uniqueSum),
+      labels: Object.keys(uniqueSum),
+      type: 'pie'
+    }];
+    
+    var pieLayout = {
+      height: 500,
+      width: 550
+    };
+    
+    Plotly.newPlot('pie', pieData, pieLayout)
+    
+    Plotly.newPlot('bubble', bubbleData, bubbleLayout,  {responsive: true})
+
+d3.json('/metadata/' + sample).then((metaData) => {
+    var gaugeData = [
+      {
+        domain: { x: [0, 1], y: [0, 1] },
+        value: metaData['WFREQ'],
+        title: { text: "Wash Frequency" },
+        type: "indicator",
+        mode: "gauge+number",
+        gauge: { axis: { visible: true, range: [0, 7] } }
+      }
+    ];
+    
+    var gaugeLayout = { width: 400, height: 500, margin: { t: 0, b: 0 } };
+
+    Plotly.newPlot('gauge', gaugeData, gaugeLayout);
+})
+
+  })
+
+
 }
 
 function init() {
-  // Grab a reference to the dropdown select element
-  var selector = d3.select("#selDataset");
 
 
   // Use the list of sample names to populate the select options
@@ -62,5 +157,4 @@ function optionChanged(newSample) {
 // Initialize the dashboard
 init();
 
-console.log('hello world')
-// selector.on('change', optionChanged(this.value))
+selector.on('change', optionChanged(this.value))
